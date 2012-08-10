05d2435a8a5d7586d38b0436eef9379b1b434c8e
System Administrator <root@Nostromo.local>
2012-08-10 16:18:29 +0200

    initial commit

---
+++ PyUp.py
@@ -0,0 +1,30 @@
+import os.path, time, sys, commands, inspect
+from xml.dom.minidom import parse, parseString
+
+uploaded_files = []
+folder_to_watch = sys.argv[1]
+upload = False
+
+print 'Hej. PyUp is now watching: ' + folder_to_watch + '!'
+
+def modification_date(filename):
+	t = os.path.getmtime(filename)
+	return datetime.datetime.fromtimestamp(t)
+
+def copy(url):
+	commands.getstatusoutput('echo "'+url.strip()+'" | pbcopy')
+	commands.getstatusoutput("osascript -e 'tell app \"System Events\" to display dialog \"Upload complete!\"' ")
+
+def upload_file(filepath):
+	output = os.popen('curl -F "image=@'+filepath+'" -F "key=47e4f6aa03fa0defcaa68bf188d8026e" http://api.imgur.com/2/upload.xml')
+	for node in parse(output).getElementsByTagName('original'):
+		copy(node.toxml()[10:-11])
+
+while upload != True:
+	for filename in os.listdir(folder_to_watch):
+		file_created_at = os.path.getmtime(folder_to_watch + '/' + filename)
+		if (time.time() - file_created_at) < 4:
+			if filename not in uploaded_files:
+				upload_file(folder_to_watch + filename)
+				uploaded_files.append(filename)
+	time.sleep(1)
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 ImgUp
 =====
 
-Automatically uploads screenshots to imgur.com and adds the image-URL the clipboard.
+PyUp automatically uploads screenshots to [imgur.com](http://imgur.com/) and adds the image-URL to the clipboard.
+
+###Usage
+
+``` $ python PyUp.py /path/to/screenshot/folder/```
+
+PyUp needs the screenshots to be saved to a dedicated location other than the desktop, as it's uplaoding every file added to this folder. 
+You can change the default location using this command:
+
+
+``` $ mkdir ~/pyup_screenshots/```<br>
+``` $ defaults write com.apple.screencapture location ~/pyup_screenshots/```
