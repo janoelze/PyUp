dbfdb8f1ee9aeb9cbd7710660eb505c85ccee3d0
Jan Oelze <janoelzelebt@googlemail.com>
2013-06-07 14:36:12 +0200

    Update PyUp.py

--- PyUp.py
+++ PyUp.py
@@ -5,14 +5,15 @@ uploaded_files = []
 folder_to_watch = sys.argv[1]
 upload = False
 
-print 'Hej. PyUp is now watching: ' + folder_to_watch + '!'
+print 'Hej. PyUp is now watching: %s!' % folder_to_watch
 
 def copy(url):
-	commands.getstatusoutput('echo "'+url.strip()+'" | pbcopy')
+	commands.getstatusoutput('echo "%s" | pbcopy' % url.strip())
 	commands.getstatusoutput("osascript -e 'tell app \"System Events\" to display dialog \"Upload complete!\"' ")
 
 def upload_file(filepath):
-	output = os.popen('curl -F "image=@'+filepath+'" -F "key=47e4f6aa03fa0defcaa68bf188d8026e" http://api.imgur.com/2/upload.xml')
+	api_key = "47e4f6aa03fa0defcaa68bf188d8026e"
+	output = os.popen('curl -F "image=@%s" -F "key=%s" http://api.imgur.com/2/upload.xml' % ( filepath, api_key ))
 	for node in parse(output).getElementsByTagName('original'):
 		copy(node.toxml()[10:-11])
 
